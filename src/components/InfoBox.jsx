d2977882d6ad7b172ec90c6940abad9d22a16372
André Sebastian <a.sebastian@posteo.de>
2019-09-28 08:01:18 +0200

    use hooks in InfoBox.jsx

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -1,21 +1,26 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import DataPresentation from './DataPresentation'
 import chatsupport from '../media/chatbot.png'
 
-export default class InfoBox extends Component {
-    render() {
-        let backendData = this.props.backendData;
-        return (backendData) 
-        ?  (
-            <div>
-                <h1>Your Orders</h1>
-                <DataPresentation data={backendData.data} />
-            </div>
-        )
-        : (
-            <div>
-                <img src={chatsupport} width="500" alt="chatsupport"/>
-            </div>
-        )
-    }
+function InfoBox (props) {
+    let [backendData, setBackendData] = useState(props.backendData)
+
+    useEffect(() => {
+        setBackendData(props.backendData)
+    }, [props.backendData])
+
+    return (backendData) 
+    ?  (
+        <div>
+            <h1>Your Orders</h1>
+            <DataPresentation data={backendData.data} />
+        </div>
+    )
+    : (
+        <div>
+            <img src={chatsupport} width="500" alt="chatsupport"/>
+        </div>
+    )
 }
+
+export default InfoBox
