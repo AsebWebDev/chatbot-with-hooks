07f6b1979fdcf5ef07f5e651c27fd3be63c70e58
André Sebastian <a.sebastian@posteo.de>
2019-09-28 09:39:17 +0200

    use hooks in Message.jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import botavatar from '../media/bot.gif'
 import useravatar from '../media/user.png'
 
-export default class Message extends Component {
-    render() {
-        return this.props.user 
-        ? (<p className="align-right message-user"><img src={useravatar} alt="" width="50"/><b>User:</b> {this.props.text}</p>)
-        : (<p className="align-left message-bot"><img src={botavatar} alt="" width="50"/><b>Botty:</b>{this.props.text}</p>)
-    }
+function Message (props) {
+        return props.user 
+        ? (<p className="align-right message-user"><img src={useravatar} alt="" width="50"/><b>User:</b> {props.text}</p>)
+        : (<p className="align-left message-bot"><img src={botavatar} alt="" width="50"/><b>Botty:</b>{props.text}</p>)
 }
+
+export default Message
