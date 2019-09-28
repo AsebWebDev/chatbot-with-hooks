448170e99578ffeed023edfb8f26b54b094b1c3f
André Sebastian <a.sebastian@posteo.de>
2019-09-28 09:49:04 +0200

    use hooks in ShippimgStatus.jsx

--- src/components/ShippingStatus.jsx
+++ src/components/ShippingStatus.jsx
@@ -1,20 +1,19 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import parseDate from '../helpers/parseDate'
 
-export default class ShippingStatus extends Component {
-    render() {
-        
-        const status = this.props.status;
-        const shipmentDate = parseDate(status.shipmentDate);
-        const lastUpdate = parseDate(status.lastUpdate);
-        const deliveryStatus = (status.status === "DELIVERED_TO_NEIGHBOUR") ? "Delivered to Neighbour" : status.status;
-        return (
-            <div>
-                <p>Shipment Date: <b>{shipmentDate}</b></p>
-                <p>Last Update: <b>{lastUpdate}</b></p>
-                <p>{deliveryStatus}</p>
-                <p>{status.extraInfo}</p>
-            </div>
-        )
-    }
+function ShippingStatus (props) {
+    let [status] = useState(props.status)
+    const shipmentDate = parseDate(status.shipmentDate);
+    const lastUpdate = parseDate(status.lastUpdate);
+    const deliveryStatus = (status.status === "DELIVERED_TO_NEIGHBOUR") ? "Delivered to Neighbour" : status.status;
+    return (
+        <div>
+            <p>Shipment Date: <b>{shipmentDate}</b></p>
+            <p>Last Update: <b>{lastUpdate}</b></p>
+            <p>{deliveryStatus}</p>
+            <p>{status.extraInfo}</p>
+        </div>
+    )
 }
+
+export default ShippingStatus
